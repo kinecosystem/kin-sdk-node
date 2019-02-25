6e4ca148f9fbbf558276dc4c29560b0e8fd465b0
Yossi Rizgan <yosriz@gmail.com>
2019-02-25 16:43:41 +0200

    implement account related methods in kin client

--- scripts/src/kinClient.ts
+++ scripts/src/kinClient.ts
@@ -3,9 +3,20 @@ import {KinAccount} from "./kinAccount";
 import {KinClientConfig} from "./KinClientConfig";
 import {AccountData, Balance} from "./blockchain/horizonModels";
 import {RawTransaction, SimplifiedTransaction} from "./transaction";
+import {Server} from "@kinecosystem/kin-sdk";
+import {Network} from "@kinecosystem/kin-base";
+import {AccountDataRetriever} from "./blockchain/accountDataRetriever";
 
 export class KinClient {
+
+	private readonly server: Server;
+	private readonly accountDataRetriever: AccountDataRetriever;
+
 	constructor(readonly environment: Environment) {
+		this.environment = environment;
+		this.server = new Server(environment.url);
+		Network.use(new Network(environment.passphrase));
+		this.accountDataRetriever = new AccountDataRetriever(this.server);
 	}
 
 	async getConfig(): Promise<KinClientConfig> {
@@ -21,15 +32,15 @@ export class KinClient {
 	}
 
 	async getAccountBalance(address: Address): Promise<Balance> {
-		return Promise.resolve(0);
+		return await this.accountDataRetriever.fetchKinBalance(address);
 	}
 
 	async isAccountExisting(address: Address): Promise<boolean> {
-		return Promise.resolve(false);
+		return await this.accountDataRetriever.isAccountExisting(address);
 	}
 
 	async getAccountData(address: Address): Promise<AccountData | null> {
-		return Promise.resolve(null);
+		return await this.accountDataRetriever.fetchAccountData(address);
 	}
 
 	async getTransactionData(txHash: string): Promise<SimplifiedTransaction | RawTransaction> {
