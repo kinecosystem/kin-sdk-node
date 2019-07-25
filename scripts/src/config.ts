4b7e8580fb309710d6b0e6d7b89071feba5acf39
Aryeh Katz <aryeh.katz@kin.org>
2019-07-25 17:33:31 +0300

    don't set headers in browser
    Setting [those] headers in the browser is forbidden and XHRs are failing.

--- scripts/src/config.ts
+++ scripts/src/config.ts
@@ -7,8 +7,11 @@ export const CHANNEL_TOP_UP_TX_COUNT = 1000;
 export const MEMO_LENGTH_ERROR: string = "Memo must be up to 28 characters, including memo prefix.";
 export const MEMO_LENGTH: number = 21;
 
-export const GLOBAL_HEADERS = new Map<string, string>()
-	.set("user-agent", "kin-sdk-node-" + PACKAGE_JSON.version)
-	.set("kin-sdk-node-version", PACKAGE_JSON.version);
 
-export const GLOBAL_RETRY =  { retries: 4 };
+export const GLOBAL_HEADERS = new Map<string, string>();
+if (typeof window === 'undefined') {
+	GLOBAL_HEADERS.set("user-agent", "kin-sdk-node-" + PACKAGE_JSON.version)
+		.set("kin-sdk-node-version", PACKAGE_JSON.version);
+}
+
+export const GLOBAL_RETRY = { retries: 4 };
